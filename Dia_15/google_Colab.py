c1e4a784854047469231d2b7290a394144dc248e
Sebastian Garcia <sgarciaar@gmail.com>
2023-06-20 16:25:06 -0400

    Dia_15

--- Dia_15/google_Colab.py
+++ Dia_15/google_Colab.py
@@ -5,6 +5,11 @@ ejecutar py
 primera clase
 https://colab.research.google.com/drive/1vAhr-0sFg7q5PmL34SggfZ9NptHuTc-k#scrollTo=z4XeFVsoviZh
 segunda clase
+https://colab.research.google.com/drive/1sCqZlCiVqtnsgBI4wNGvgmm3QXYMaq_u
 
+cuarta
+https://colab.research.google.com/drive/1UzX5iH2ce0m97zi-152iWBzClWRdiuEJ#scrollTo=kjHarWuXP3c-
 
+machinne learning
+https://colab.research.google.com/drive/1Tzb7Nl2PSnEemrwFqY2eb97qaRglJDLq
 '''
